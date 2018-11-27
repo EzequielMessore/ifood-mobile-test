e4fd7348e05ec017572668a378087ba20193ea7f
Ezequiel Messore <ezequiel.messore@s2it.com.br>
2018-11-27 11:21:07 -0200

    Adding base project

---
+++ app/src/main/java/br/com/ezequiel/twitterhappines/AndroidApplication.kt
@@ -0,0 +1,9 @@
+package br.com.ezequiel.twitterhappines
+
+import android.app.Application
+
+class AndroidApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
