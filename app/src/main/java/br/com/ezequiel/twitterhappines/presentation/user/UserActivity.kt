b35570c2ba923b42498d93865482b050eb4b9aec
Ezequiel Messore <ezequiel.messore@s2it.com.br>
2018-12-01 19:07:21 -0200

    Refactor UserActivity

--- app/src/main/java/br/com/ezequiel/twitterhappines/presentation/user/UserActivity.kt
+++ app/src/main/java/br/com/ezequiel/twitterhappines/presentation/user/UserActivity.kt
@@ -3,14 +3,19 @@ package br.com.ezequiel.twitterhappines.presentation.user
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.support.design.widget.Snackbar
 import br.com.ezequiel.twitterhappines.R
 import br.com.ezequiel.twitterhappines.core.extension.contentView
 import br.com.ezequiel.twitterhappines.core.platform.InjectableActivity
 import br.com.ezequiel.twitterhappines.databinding.ActivityUserBinding
 import br.com.ezequiel.twitterhappines.presentation.tweet.TweetActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_user.*
 import kotlinx.android.synthetic.main.base_content.*
 import kotlinx.android.synthetic.main.container_error.view.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class UserActivity : InjectableActivity() {
@@ -58,20 +63,23 @@ class UserActivity : InjectableActivity() {
 
     private fun handleUserState(state: UserState) {
         when (state) {
-            is UserLoading -> {
-                loading.show()
-            }
             is UserData -> {
-                state.data?.let {
-                    startActivity(TweetActivity.newIntent(this, it))
-                    loading.hide()
+                state.data?.let { user ->
+                    Snackbar.make(content, getString(R.string.msg_success_user), Snackbar.LENGTH_LONG).show()
+
+                    Observable
+                        .timer(1500, TimeUnit.MILLISECONDS)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe {
+                            startActivity(TweetActivity.newIntent(this, user))
+                        }.dispose()
                 }
             }
             is UserError -> {
                 if (state.isNotFound()) {
                     til_search.error = getString(R.string.label_user_not_found)
                 }
-                loading.hide()
             }
         }
     }
